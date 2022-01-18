dd7ea3cfc258282f3c00dd8ea86c1a78e3fdcbb2
MS-IRYWUODNDPBK\Administrator <407818306@qq.com>
2022-01-18 22:23:40 +0800

    。

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -49,7 +49,7 @@
     <Button theme="link" disabled>禁用链接按钮</Button>
     <Button theme="text" disabled>禁用按钮</Button>
   </div>
-  <h1>示例5</h1>
+  <h1>加载</h1>
   <div>
     <Button loading>加载中</Button>
     <Button>加载完毕</Button>
