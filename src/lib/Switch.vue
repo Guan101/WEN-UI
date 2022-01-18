1160860d5fb30b6620b6e11b9e57924c6431263e
MS-IRYWUODNDPBK\Administrator <407818306@qq.com>
2022-01-18 21:45:35 +0800

    button基本完成

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,48 +1,40 @@
 <template>
-  <button @click="toggle" :class="{checked:value}"> <span></span></button>
+  <button class="guan-switch" @click="toggle" :class="{'guan-checked':value}">
+    <span></span>
+  </button>
 </template>
 <script lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 export default {
-    props: {
+  props: {
     value: Boolean,
   },
-  setup(props, context){
-    const toggle = ()=>{
-      context.emit('update:value', !props.value)
-    }
-    return { toggle}
-  } 
-}
+  setup(props, context) {
+    const toggle = () => {
+      context.emit("update:value", !props.value);
+    };
+    return { toggle };
+  }
+};
 </script>
-<style lang="scss" scoped>
-button {
-  height: 22px; 
-  width: 44px;
-  border: none;
-  background: #bfbfbf;
-  border-radius: 11px; 
-  position: relative;
+
+<style lang="scss">
+$h: 22px;
+$h2: $h - 4px;
+.guan-switch {
+  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
   > span {
-    position: absolute; 
-    top: 2px; 
-    left: 2px; 
-    height: 18px; 
-    width: 18px;
-    background: white; 
-    border-radius: 9px;
-    transition: all 250ms;
-    caret-color: transparent;
+    position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
   }
-  &.checked { background: #1890ff;
-    > span { left: calc(100% - 19px)}
+  &.guan-checked { background: #1890ff;
+    > span { left: calc(100% - #{$h2} - 2px); }
   }
-  &:focus{outline: none;}
-  &:active{
-    > span {width: 22px;}
+  &:focus { outline: none; }
+  &:active {
+    > span { width: $h2 + 4px; }
   }
- &.checked:active{
-    > span {width: 22px; margin-left: -4px;}
+  &.guan-checked:active {
+    > span { width: $h2 + 4px; margin-left: -4px; }
   }
 }
-</style> 
+</style>
