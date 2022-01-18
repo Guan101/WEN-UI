b9460a8f54df0d32f4766a5d0ce64b05322e71b6
MS-IRYWUODNDPBK\Administrator <407818306@qq.com>
2022-01-18 15:36:09 +0800

    TOPNAV样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+       <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <div class="banner">
-      <h1>轱辘UI</h1>
+      <h1>GUAN-UI</h1>
       <h2>一个厉害的 UI 框架</h2>
       <p class="actions">
         <a>GitHub</a>
@@ -15,6 +18,21 @@
   </div>
 </template>
 <style lang="scss" scoped>
-  .topnar{border: 1px solid red;}
-  
+  .topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
 </style> 
