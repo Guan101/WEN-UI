580c83185360f12561f32bb8f71a0b28dbf66e8e
MS-IRYWUODNDPBK\Administrator <407818306@qq.com>
2022-01-18 17:01:41 +0800

    完善aside切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+  <span class="toggleAside" @click="toggleMenu">kk</span>
   </div>
-  <span class="toggleAside"></span>
 </template>
 <script lang="ts">
 import { inject, Ref } from "vue"
@@ -42,10 +42,20 @@ export default {
       margin: 0 1em;
     }
   }
-   > .toggleMenu {}
+    .toggleAside {
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: none;
+  }
   @media (max-width:500px) {
     > .menu{display: none;} 
     > .logo{margin: 0 auto;}
+    >.toggleAside{display:inline-block}
   }
 }
 </style> 
