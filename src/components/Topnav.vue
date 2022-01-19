298f3fa894d0225d95b205d91baf1f0330e99406
MS-IRYWUODNDPBK\Administrator <407818306@qq.com>
2022-01-19 21:38:06 +0800

    ICON引入

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,10 @@
 <template>
  <div class="topnav">
-  <div class="logo">LOGO</div>
+   <div class="logo">
+    <svg class="icon">
+      <use xlink:href="#icon-king"></use>
+    </svg>
+  </div>
   <ul class="menu">
     <li>菜单1</li>
     <li>菜单2</li>
@@ -22,7 +26,9 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+$color: #007974;
 .topnav {
+  color: $color;
   display: flex;
   padding: 16px;
   position: fixed;
@@ -35,6 +41,10 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    >svg {
+      width: 32px;
+      height: 32px;
+    }
   }
   > .menu {
     display: flex;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
+<div>
   <div class="topnavAndBanner">
-  <Topnav />
-  <div class="banner">
-    <h1>GUAN-UI</h1>
-    <h2>一个厉害的 UI 框架</h2>
-    <p class="actions">
-      <a href="https://github.com">GitHub</a>
-      <router-link to="/doc">开始</router-link>
-    </p>
+    <Topnav />
+    <div class="banner">
+      <h1>GUAN-UI</h1>
+      <h2>一个厉害的 UI 框架</h2>
+      <p class="actions">
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
+      </p>
+    </div>
+  </div>
+  <div class="features">
+    <svg class="icon">
+      <use xlink:href="#icon-vue"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-ts"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-light"></use>
+    </svg>
   </div>
   </div>
 </template>
@@ -18,10 +31,21 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
 .topnavAndBanner {
   background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
 }
+.features {
+  >svg {
+    width: 64px;
+    height: 64px;
+  }
+}
+
  .banner {
+  color: $color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
@@ -31,12 +55,14 @@ export default {
     padding: 8px 0;
     a {
       margin:0 8px;
-      background: #fff;
+      background: $green;
+      color: white;
       display: inline-block;
-      height: 28px;
-      line-height:28px;
-      border-radius: 14px;
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
