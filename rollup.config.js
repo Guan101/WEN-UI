4b07c1ed9074e418bb941b93aeee8b74059f3729
MS-IRYWUODNDPBK\Administrator <407818306@qq.com>
2022-01-21 13:53:43 +0800

    ...

--- rollup.config.js
+++ rollup.config.js
@@ -12,13 +12,13 @@ export default {
     globals: {
       vue: 'Vue'
     },
-    name: 'Gulu',
-    file: 'dist/lib/gulu.js',
+    name: 'Wen',
+    file: 'dist/lib/Wen.js',
     format: 'umd',
     plugins: [terser()]
   }, {
-    name: 'Gulu',
-    file: 'dist/lib/gulu.esm.js',
+    name: 'Wen',
+    file: 'dist/lib/Wen.esm.js',
     format: 'es',
     plugins: [terser()]
   }],
